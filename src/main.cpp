3ecb1bc8fa1bddaea6f7ffc1412d188931a2cdca
Jordan <j.clement@bmspowersafe.com>
2026-06-26 09:02:59 +0200

    update exemple

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,12 @@
 #include <Arduino.h>
 
-#define LED_GREEN 2   // CMD_LED_GREEN
-#define LED_RED   16  // CMD_LED_RED
-
 void setup() {
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_RED, OUTPUT);
   Serial.begin(115200);
+  delay(1000);
   Serial.println("BOOT OK");
 }
 
 void loop() {
-  digitalWrite(LED_GREEN, HIGH);
-  Serial.println("LED GREEN ON");
-  delay(500);
-
-  digitalWrite(LED_GREEN, LOW);
-  digitalWrite(LED_RED, HIGH);
-  Serial.println("LED RED ON");
-  delay(500);
-
-  digitalWrite(LED_RED, LOW);
+  Serial.println("HELLO");
+  delay(1000);
 }
